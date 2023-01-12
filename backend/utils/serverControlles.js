96f78145993ab115bd43eef215e084f84dfec0b7
Radek <plocharadoslaw@gmail.com>
2023-01-12 15:24:28 +0100

    fixed saving messages to db with already existing pair

--- backend/utils/serverControlles.js
+++ backend/utils/serverControlles.js
@@ -1,20 +1,36 @@
 const { ChatRoom, userPairs } = require("../models/chatModel");
 
 const saveMessagesToDB = (username, messages, room, pair) => {
-  if (messages) {
-    ChatRoom.findOne({ username: username }, (err, chat) => {
-      if (!chat) {
-        ChatRoom.create({
-          username: username,
-          roomMessages: [{ pair: pair, messages: messages }],
-        });
-      } else {
-        chat.roomMessages.push({ pair: pair, messages: messages });
-        chat.save((err, updated) => {
-          if (err) console.log(err);
-        });
-      }
-    });
+  console.log(username, pair);
+  if (pair && username !== pair) {
+    if (messages) {
+      ChatRoom.findOne({ username: username }, (err, chat) => {
+        if (!chat) {
+          ChatRoom.create({
+            username: username,
+            roomMessages: [{ pair: pair, messages: messages }],
+          });
+        } else {
+          let alreadyExists = false;
+          chat.roomMessages.forEach((el) => {
+            if (el.pair === pair) {
+              alreadyExists = true;
+            }
+          });
+          if (alreadyExists) {
+            chat.roomMessages.forEach((el) => {
+              console.log(el.pair, pair);
+              if (el.pair === pair) el.messages.push(...messages);
+            });
+          } else {
+            chat.roomMessages.push({ pair: pair, messages: messages });
+          }
+          chat.save((err, updated) => {
+            if (err) console.log(err);
+          });
+        }
+      });
+    }
   }
 };
 const saveUserPairsToDB = (username, newPairs) => {
